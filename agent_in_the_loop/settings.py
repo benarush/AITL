ddef8648df06e95813bfe5b08c773d8d929c1b02
Tomer Ben Harush <benharushtomer@gmail.com>
2026-07-03 19:10:20 +0300

    change default endpoint

--- agent_in_the_loop/settings.py
+++ agent_in_the_loop/settings.py
@@ -3,10 +3,7 @@ from typing import Optional
 
 ENV_AITL_API_KEY = "AGENT_IN_THE_LOOP_API_KEY"
 
-# Fixed backend domain. Not configurable via env var or caller-supplied
-# argument so clients can't point the SDK at an arbitrary/untrusted host.
-AITL_ENDPOINT = "https://trellar.io"
-
+DEFAULT_ENDPOINT = "https://api.trellar.io/"
 
 def get_env_api_key() -> Optional[str]:
     return os.getenv(ENV_AITL_API_KEY)
