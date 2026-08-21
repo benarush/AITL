d432cd469be0e1b2829723fe6f62cb080a65e8b9
Tomer Ben Harush <benharushtomer@gmail.com>
2026-08-21 03:02:55 +0300

    fix

--- agent_in_the_loop/settings.py
+++ agent_in_the_loop/settings.py
@@ -3,8 +3,8 @@ from typing import Optional
 
 ENV_AITL_API_KEY = "AGENT_IN_THE_LOOP_API_KEY"
 
-#DEFAULT_ENDPOINT = "https://api.trellar.io/"
-DEFAULT_ENDPOINT = "http://localhost:8001"
+DEFAULT_ENDPOINT = "https://api.trellar.io/"
+# DEFAULT_ENDPOINT = "http://localhost:8001"
 
 def get_env_api_key() -> Optional[str]:
     return os.getenv(ENV_AITL_API_KEY)
