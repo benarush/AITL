713b06c1ee4f359d8b668c90961f700d91af877b
Tomer Ben Harush <benharushtomer@gmail.com>
2026-09-22 01:41:19 +0300

    remove that settings

--- trellar/settings.py
+++ trellar/settings.py
@@ -4,7 +4,7 @@ from typing import Optional
 ENV_TRELLAR_API_KEY = "TRELLAR_API_KEY"
 
 DEFAULT_ENDPOINT = "https://api.trellar.io/"
-DEFAULT_ENDPOINT = "http://localhost:8001"
+# DEFAULT_ENDPOINT = "http://localhost:8001"
 
 def get_env_api_key() -> Optional[str]:
     return os.getenv(ENV_TRELLAR_API_KEY)
